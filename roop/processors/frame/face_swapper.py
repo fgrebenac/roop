7230ed4d3777d3ffba7e3d55c922e5dc5be61614
Filip Grebenac <fgrebenac@gmail.com>
2023-09-27 12:40:49 +0200

    Change model download path

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -21,7 +21,7 @@ def get_face_swapper() -> Any:
 
     with THREAD_LOCK:
         if FACE_SWAPPER is None:
-            model_path = resolve_relative_path('../models/inswapper_128.onnx')
+            model_path = '/shared/roop/models/inswapper_128.onnx'
             FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.execution_providers)
     return FACE_SWAPPER
 
@@ -33,7 +33,7 @@ def clear_face_swapper() -> None:
 
 
 def pre_check() -> bool:
-    download_directory_path = resolve_relative_path('../models')
+    download_directory_path = '/shared/roop/models'
     conditional_download(download_directory_path, ['https://huggingface.co/CountFloyd/deepfake/resolve/main/inswapper_128.onnx'])
     return True
 
