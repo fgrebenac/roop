bbb35686b54632886ad4d86cafbd9cbf7bb26850
Filip Grebenac <fgrebenac@gmail.com>
2023-09-28 14:32:10 +0200

    Download to /models

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -5,6 +5,7 @@ import numpy
 
 import roop.globals
 from roop.typing import Frame, Face
+from roop.utilities import resolve_relative_path
 
 FACE_ANALYSER = None
 THREAD_LOCK = threading.Lock()
@@ -15,7 +16,7 @@ def get_face_analyser() -> Any:
 
     with THREAD_LOCK:
         if FACE_ANALYSER is None:
-            FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', providers=roop.globals.execution_providers)
+            FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', providers=roop.globals.execution_providers, root='/shared/roop')
             FACE_ANALYSER.prepare(ctx_id=0)
     return FACE_ANALYSER
 
